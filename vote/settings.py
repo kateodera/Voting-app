a70620e1d13b9180991b174a1fb16929c9f3123a
Kate <akinyi180@gmail.com>
2019-12-16 12:30:10 +0300

    debug set to false

--- vote/settings.py
+++ vote/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*&44+$^6gay)hfngku*fp5&3ur0kumr%fs4iy!y^!t%a9x=*wh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
